d9d57b8568d8157360986324d3bb97b20e7b8fe2
ducoffeM <melanie.ducoffe@airbus.com>
2024-11-07 08:36:25 +0100

    documentation for backward constant

--- keras_custom/backward/custom/constant.py
+++ keras_custom/backward/custom/constant.py
@@ -4,6 +4,19 @@ from keras_custom.backward.layer import BackwardLinearLayer
 
 
 class BackwardMulConstant(BackwardLinearLayer):
+    """
+    This class implements a custom layer for backward pass of a `MulConstant` layer in Keras.
+    It can be used to apply operations in a reverse manner back to the original input shape.
+
+    ### Example Usage:
+    ```python
+    from keras.layers import MulConstant
+    from keras_custom.backward.layers import BackwardMulConstant
+
+    # Assume `mulconst_layer` is a pre-defined MulConstant layer
+    backward_layer = BackwardMulConstant(conv_layer)
+    output = backward_layer(input_tensor)
+    """
     def __init__(
         self,
         layer: MulConstant,
@@ -18,11 +31,46 @@ class BackwardMulConstant(BackwardLinearLayer):
 
 
 def get_backward_MulConstant(layer: MulConstant, use_bias=True) -> Layer:
+    """
+    This function creates a `BackwardMulConstant` layer based on a given `MulConstant` layer. It provides
+    a convenient way to obtain the ackward pass of the input `MulConstant` layer, using the
+    `BackwardMulConstant` class to reverse the operation.
+
+    ### Parameters:
+    - `layer`: A Keras `MulConstant` layer instance. The function uses this layer's configurations to set up the `BackwardMulConstant` layer.
+    - `use_bias`: Boolean, optional (default=True). Specifies whether the bias should be included in the
+      backward layer.
+
+    ### Returns:
+    - `layer_backward`: An instance of `BackwardMulConstant`, which acts as the reverse layer for the given `MulConstant`.
+
+    ### Example Usage:
+    ```python
+    from keras.layers import MulConstant
+    from keras_custom.backward import get_backward_MulConstant
+
+    # Assume `mulconst_layer` is a pre-defined MulConstant layer
+    backward_layer = get_backward_MulConstant(mulconst_layer, use_bias=True)
+    output = backward_layer(input_tensor)
+    """
 
     return BackwardMulConstant(layer)
 
 
 class BackwardPlusConstant(BackwardLinearLayer):
+    """
+    This class implements a custom layer for backward pass of a `PlusConstant` layer in Keras.
+    It can be used to apply operations in a reverse manner back to the original input shape.
+
+    ### Example Usage:
+    ```python
+    from keras.layers import PlusConstant
+    from keras_custom.backward.layers import BackwardPlusConstant
+
+    # Assume `plusconst_layer` is a pre-defined PlusConstant layer
+    backward_layer = BackwardPlusConstant(conv_layer)
+    output = backward_layer(input_tensor)
+    """
     def __init__(
         self,
         layer: PlusConstant,
@@ -42,4 +90,26 @@ class BackwardPlusConstant(BackwardLinearLayer):
 
 
 def get_backward_PlusConstant(layer: PlusConstant, use_bias=True) -> Layer:
+    """
+    This function creates a `BackwardPlusConstant` layer based on a given `PlusConstant` layer. It provides
+    a convenient way to obtain the ackward pass of the input `PlusConstant` layer, using the
+    `BackwardPlusConstant` class to reverse the operation.
+
+    ### Parameters:
+    - `layer`: A Keras `PlusConstant` layer instance. The function uses this layer's configurations to set up the `BackwardPlusConstant` layer.
+    - `use_bias`: Boolean, optional (default=True). Specifies whether the bias should be included in the
+      backward layer.
+
+    ### Returns:
+    - `layer_backward`: An instance of `BackwardPlusConstant`, which acts as the reverse layer for the given `PlusConstant`.
+
+    ### Example Usage:
+    ```python
+    from keras.layers import PlusConstant
+    from keras_custom.backward import get_backward_PlusConstant
+
+    # Assume `plusconst_layer` is a pre-defined PlusConstant layer
+    backward_layer = get_backward_PlusConstant(plusconst_layer, use_bias=True)
+    output = backward_layer(input_tensor)
+    """
     return BackwardPlusConstant(layer, use_bias)
